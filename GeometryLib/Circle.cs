ff76b184c08998d512855fbffc8d70d715f0a6f5
mekrav <mekrav@gmail.com>
2022-11-09 14:14:38 +0700

    Добавлен класс круга и вычисление его площади

--- GeometryLib/Circle.cs
+++ GeometryLib/Circle.cs
@@ -4,5 +4,26 @@ namespace GeometryLib
 {
     internal class Circle
     {
+        public double radius
+        {
+            get
+            {
+                return radius;
+            }
+            set
+            {
+                if (value >= 0)
+                    radius = value;
+            }
+        }
+
+        /// <summary>
+        /// Вычисляет площадь круга
+        /// </summary>
+        public double Area()
+        {
+            // Можно так же использовать Math.Power()
+            return Math.PI * radius * radius;
+        }
     }
 }
